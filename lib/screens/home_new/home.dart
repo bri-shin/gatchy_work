73db486276a050cdaa613c4461f310d6d015451f
Brian Shin <bri-shin@Brians-MacBook-Pro.local>
2020-03-13 21:57:55 +0900

    habit card implemented

--- lib/screens/home_new/home.dart
+++ lib/screens/home_new/home.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:gatchy_work/models/HabitCardModel.dart';
+
 
 
 class gatchyHome extends StatefulWidget {
@@ -7,6 +9,15 @@ class gatchyHome extends StatefulWidget {
 }
 
 class _gatchyHomeState extends State<gatchyHome> with TickerProviderStateMixin{
+
+  var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0), Color.fromRGBO(111, 194, 173, 1.0)];
+  var cardIndex = 0;
+  ScrollController scrollController;
+  var currentcolor = Colors.white;
+
+  var habitList = [HabitCardModel("Workout", Icons.directions_run, 45, 27), HabitCardModel("Read", Icons.chrome_reader_mode , 45, 27), HabitCardModel("Save", Icons.monetization_on , 45, 27)];
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -47,7 +58,79 @@ class _gatchyHomeState extends State<gatchyHome> with TickerProviderStateMixin{
                   ],
                   ),
                 )
-              )
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                    child: Text("Today : MARCH 12, 2020", style: TextStyle(color: Colors.black),),
+                    ),
+                  Container(
+                    height: 350.0,
+                    child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 3,
+                      controller: scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, position) {
+                        return GestureDetector(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: Container(
+                                width: 250.0,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(15.0),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Icon(habitList[position].icon, color: appColors[position],),
+                                          Icon(Icons.more_vert, color: Colors.grey),
+                                        ],),),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+                                            child: Text("${habitList[position].habitTitle}", style: TextStyle(fontSize: 30.0),),
+                                            ),
+                                          Padding(
+                                            padding:const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: <Widget>[
+                                              Text("\$ ${habitList[position].moneyEarned} earned", style: TextStyle(color: Colors.grey[800],),),
+                                              Text("\$ ${habitList[position].streak} Days", style: TextStyle(color: Colors.grey[800],),)
+                                                ],),
+                                            ),
+                                        ],
+                                        )
+                                      )
+                                  ],
+                                  )
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)
+                                ),
+                            )
+                            ),
+                            onHorizontalDragEnd: (details) {
+                              
+                            }
+                          ,);
+                      }
+                      
+
+                      ,)
+                  )
+                ],)
           ],)
       )
     );
